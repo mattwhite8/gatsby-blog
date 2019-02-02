6a7b55a27fe33c1953bcbe5e53495645e5fcfec7
Matthew White <matthew.white697@gmail.com>
2019-02-02 14:46:59 -0500

    homepage footer

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import About from './About'
+import Footer from './Footer'
 import './style.scss'
 
 const Layout = ({ children }) => (
     <div>
         <Hero />
         <About />
+        <Footer />
     </div>
 )
 
