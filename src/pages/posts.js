62f0f98e2f868002bd1f836c864a9d05837686e3
Matt White <mattwhite1337@gmail.com>
2019-02-04 12:18:44 -0500

    index page for posts

---
+++ src/pages/posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Navbar'
+
+export default () => (
+    <div>
+        <Nav />
+        <section className="section">
+            <div className="container">
+                <h1>Testing</h1>
+            </div>
+        </section>
+    </div>
+)
