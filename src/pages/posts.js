e8ada145ec3519994a9e41c7872ced0b5c223a58
Matthew White <matthew.white697@gmail.com>
2019-02-17 10:03:46 -0500

    link to posts

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Page from '../components/IndexPage'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
 export default ({ data }) => {
     console.log(data.allMarkdownRemark)
@@ -9,15 +9,20 @@ export default ({ data }) => {
         <Page>
             <section className="section">
                 <div className="container">
-                    <h1>Testing</h1>
-                    <div className="tile is-ancestor">
-                        {nodes.map(({ node }) => (
-                            <div className="tile is-parent">
-                                <article className="tile is-child box">
-                                    <p>{node.excerpt}</p>
-                                </article>
+                    <div className="columns is-mobile">
+                        <div className="column is-half is-offset-one-quarter">
+                            <h1>Testing</h1>
+                            <div className="tile is-ancestor">
+                                {nodes.map(({ node }) => (
+                                    <div className="tile is-parent">
+                                        <article className="tile is-child box">
+                                            <p>{node.excerpt}</p>
+                                            <Link to={node.fields.slug}>Read</Link>
+                                        </article>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                        </div>
                     </div>
                 </div>
             </section>
@@ -35,6 +40,9 @@ export const query = graphql`
                 frontmatter {
                   title
                   date
+                }
+                fields {
+                    slug
                 }
                 excerpt
               }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import Nav from '../components/Navbar'
 import { graphql } from 'gatsby'
 
 export default ({ data }) => {
     const post = data.markdownRemark
     return (
-        <section className="section">
-            <div className="container">
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-            </div>     
-        </section>
+        <div>
+            <header>
+                <section className="hero is-medium is-light is-bold">
+                    <div className="hero-head">
+                        <Nav />
+                    </div>
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                My Posts
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+            </header>
+            <section className="section">
+                <div className="container">
+                    <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                </div>     
+            </section>
+        </div>
     )
 }
 
