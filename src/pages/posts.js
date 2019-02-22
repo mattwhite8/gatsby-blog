82ad56dbf89ea349e0e59e08e92fde01a59c9890
Matthew White <matthew.white697@gmail.com>
2019-02-21 19:57:32 -0500

    minor graphql correction

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -50,7 +50,6 @@ export const query = graphql`
                 id
                 frontmatter {
                   title
-                  date
                 }
                 fields {
                     slug
