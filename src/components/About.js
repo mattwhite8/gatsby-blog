4de503393b2fecc8760a4a3cfa30a72d9825b5a8
Matthew White <matthew.white697@gmail.com>
2019-02-21 19:55:51 -0500

    changed copy

--- src/components/About.js
+++ src/components/About.js
@@ -23,9 +23,8 @@ export default () => (
                 <div className="card-content has-text-centered">
                     <h1 className="title">Hello World</h1>
                     <p>First and foremost, I love to learn.</p>
-                    <p>There was once a point in my life where the mastery of programming was my primary goal. Now that I'm a little older, I realize that is the wrong way of looking at things. When you're pursuing knowledge, a tenet of programming, there is no mastery, no final level. We often want to view the pursuit as if we were climbing a mountain, except there is no pinnacle. All we have is an endless series of trails and plateaus -- we're all a bit like Sisyphus in that regard.</p>
-                    <p>In reality, this is a good thing. I believe that we can find joy in the constant improvment we make to ourselves. Every time we reach that plateau, we take stock of our accomplishments and strive on towards the next goal. Every time we take pride in pushing ourself just a little further than we thought possible. Sitting at the top would make us boring, and we would lose that creative spark that drives us programmers to learn.</p>
-                    <p>This blog is the product of me wanting to push myself not only mentally, but physically as well. I hope you'll stick with me as I prepare for my very firs Spartan Race.</p>
+                    <p>Coming from a background with no coding experience to working fulltime as a developer has been one of the most challenging and rewarding experiences of my life. Several years ago I never would have pictured myself writing this, let alone writing this on a website that I created. I strive every day to push myself just a little further than the last, and I know I have so much further to go. Despite that, I have learned to embrace the struggle and the endless pursuit of knowledge.</p>
+                    <p>Follow me as I strive to push myself as hard physically as I have mentally. This April I will participate in the Charlotte Spartan Race -- a first for me. I will update this blog with my training routine and various thoughts and musings.</p>
                 </div>
             </div>
         </div>
--- src/posts/test.md
+++
@@ -1,8 +0,0 @@
----
-title: "Testing"
-date: "2019-02-06"
----
-
-# Hello World
-
-I am a markdown file, that's pretty cool right?
