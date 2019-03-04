a7e12d055ba55925d784df9e1e8a4f8850730cc5
Matthew White <matthew.white697@gmail.com>
2019-03-03 20:49:24 -0500

    new post

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ export default () => (
                 <div className="card-content has-text-centered">
                     <h1 className="title">Hello World</h1>
                     <p>First and foremost, I love to learn.</p>
-                    <p>Coming from a background with no coding experience to working fulltime as a developer has been one of the most challenging and rewarding experiences of my life. Several years ago I never would have pictured myself writing this, let alone writing this on a website that I created. I strive every day to push myself just a little further than the last, and I know I have so much further to go. Despite that, I have learned to embrace the struggle and the endless pursuit of knowledge.</p>
+                    <p>Coming from a background with no coding experience to working full time as a developer has been one of the most challenging and rewarding experiences of my life. Several years ago I never would have pictured myself writing this, let alone writing this on a website that I created. I strive every day to push myself just a little further than the last, and I know I have so much further to go. Despite that, I have learned to embrace the struggle and the endless pursuit of knowledge.</p>
                     <p>Follow me as I strive to push myself as hard physically as I have mentally. This April I will participate in the Charlotte Spartan Race -- a first for me. I will update this blog with my training routine and various thoughts and musings.</p>
                 </div>
             </div>
